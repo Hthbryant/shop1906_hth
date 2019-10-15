6be8d34e96fd573460061ea790888601a7912a16
hth <1635618490@qq.com>
2019-10-15 19:39:18 +0800

    实现了solr搜索商品,高亮显示

--- shop_web/shop_searchtest/src/main/java/com/qf/service/impl/SearchServiceImpl.java
+++ shop_web/shop_searchtest/src/main/java/com/qf/service/impl/SearchServiceImpl.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SearchServiceImpl implements ISearchService {
@@ -64,9 +65,21 @@ public class SearchServiceImpl implements ISearchService {
         }
 
 
+        //进行高亮设置
+        solrQuery.setHighlight(true);
+        solrQuery.setHighlightSimplePre("<font color='red'>");
+        solrQuery.setHighlightSimplePost("</font>");
+        solrQuery.addHighlightField("subject");
+
+
+
+        //开始查询
         try {
             QueryResponse response = solrClient.query(solrQuery);
             SolrDocumentList results = response.getResults();
+
+
+            Map<String, Map<String, List<String>>> highlighting = response.getHighlighting();
 
             List<Goods> goodsList = new ArrayList<>();
             for (SolrDocument document : results) {
@@ -78,6 +91,17 @@ public class SearchServiceImpl implements ISearchService {
                 goods.setFengmian(document.getFieldValue("images")+"");
                 goods.setPrice(BigDecimal.valueOf((double)document.getFieldValue("price")));
                 goods.setStorage((int)document.getFieldValue("storage"));
+
+                if(highlighting.containsKey(goods.getId()+"")){
+
+                    Map<String, List<String>> stringListMap = highlighting.get(goods.getId() + "");
+                    if(stringListMap.containsKey("subject")){
+
+                        //获得标题的高亮
+                        String subject = stringListMap.get("subject").get(0);
+                        goods.setSubject(subject);//将有高亮信息的标题放入商品对象中
+                    }
+                }
 
                 goodsList.add(goods);
             }
